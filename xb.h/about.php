2d44baddd989fe41b59400dedd3628d5bd203ad0
beixbh <107980597+beixbh@users.noreply.github.com>
2022-07-13 22:04:40 -0700

    Add files via upload

---
+++ xb.h/about.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>About Allbags</title>
+
+	<?php include "parts/meta.php"; ?>
+</head>
+<body>
+	<?php include "parts/navbar.php"; ?>
+
+	<div class="container">
+		<div class="card soft">
+			<h2>About Allbags</h2>
+
+			<p>This is information about Allbags # <?= $_GET['id'] ?>
+			</p>
+		</div>
+	</div>
+	
+</body>
+</html>
---
+++ xb.h/meta.php
@@ -0,0 +1,6 @@
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="lib/css/styleguide1.css">
+	<link rel="stylesheet" href="lib/css/gridsystem.css">
+	<link rel="stylesheet" href="css/storetheme.css">

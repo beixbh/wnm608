5254a0bef8fcbb7ca47a0b53526fdf2ed3f64c37
beixbh <107980597+beixbh@users.noreply.github.com>
2022-08-06 20:45:00 -0700

    Add files via upload

--- xb.h/admin/index.php
+++ xb.h/admin/index.php
@@ -221,6 +221,9 @@ HTML;
 	<title>Product Admin Page</title>
 
 	<?php include "../parts/meta.php"; ?>
+	<link rel="stylesheet" href="lib/css/styleguide.css">
+    <link rel="stylesheet" href="lib/css/gridsystem.css">
+	<link rel="stylesheet" href="css/storetheme.css">
 </head>
 <body>
 
--- xb.h/lib/php/functions.php
+++ xb.h/lib/php/functions.php
@@ -13,69 +13,81 @@ function file_get_json($filename) {
       return json_decode($file);
 }
 
+
+
+
+
 include_once "auth.php";
-function makeConn(){
+function makeConn() {
       $conn = new mysqli(...MYSQLIAuth());
       if($conn->connect_errno) die($conn->connect_error);
       $conn->set_charset('utf8');
       return $conn;
 }
+function makePDOConn() {
+      try {
+            $conn = new PDO(...PDOAuth());
+      } catch(PDOException $e) {
+            die($e->getMessage());
+      }
+      return $conn;
+}
 
-function makeQuery ($conn,$qry){
+
+
+function makeQuery($conn,$qry) {
       $result = $conn->query($qry);
-      if($conn->errno)die($conn->error);
+      if($conn->errno) die($conn->error);
       $a = [];
-      while($row = $result->fetch_object()){
+      while($row = $result->fetch_object()) {
             $a[] = $row;
       }
       return $a;
 }
 
 
-// CART FUNCTIONS
 
 
 
-function array_find($array,$fn){
-	foreach($array as $o) if($fn($o)) return $o;
-	return false;
+/* CART FUNCTIONS */
+
+function array_find($array,$fn) {
+      foreach($array as $o) if($fn($o)) return $o;
+      return false;
 }
-function getCart(){
-	return isset($_SESSION['cart']) ? $_SESSION['cart'] : [];
+function getCart() {
+      return isset($_SESSION['cart']) ? $_SESSION['cart'] : [];
 }
-
 function addToCart($id,$amount,$color) {
-    $cart = getCart();
+      $cart  = getCart();
 
-	$p = array_find($cart,function($o) use($id) {return $o->id==$id; });
+      $p = array_find($cart,function($o) use($id) { return $o->id==$id; });
 
-	if ($p) {
-        $p->amount += $amount;
-    } else {
-		$_SESSION['cart'][] = (object)[
-        "id"=>$id,
-        "amount"=>$amount,
-        "color"=>$color
-        ];
-    }
+      if($p) {
+            $p->amount += $amount;
+      } else {
+            $_SESSION['cart'][] = (object)[
+                  "id"=>$id,
+                  "amount"=>$amount,
+                  "color"=>$color
+            ];
+      }
 }
-
-function resetCart(){ 
-      $_SESSION['cart']=[];
-}
+function resetCart() { $_SESSION['cart'] = []; }
 
 function cartItemById($id) {
-      return array_find(getCart(),function($o) use($id) {return $o->id==$id;});
+      return array_find(getCart(),function($o)use($id){return $o->id==$id;});
 }
- 
+
 function makeCartBadge() {
       $cart = getCart();
-      if (count($cart) == 0) {
+      if(count($cart)==0) {
             return "";
       } else {
             return array_reduce($cart,function($r,$o){return $r+$o->amount;},0);
       }
 }
+
 
 function getCartItems() {
       $cart = getCart();
@@ -90,5 +102,5 @@ function getCartItems() {
             $o->amount = $p->amount;
             $o->total = $p->amount * $o->price;
             return $o;
-      }, $data);
+      },$data);
 }
